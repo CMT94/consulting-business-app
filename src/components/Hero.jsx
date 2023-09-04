f897a0d8f8a2991eaa7e605bf543f7e8da698692
Clément MGT <cmt94.dev@gmail.com>
2023-09-04 18:21:19 +0200

    Hero desktop + mobile v1 done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import classes from "./Hero.module.css";
+import globalClasses from "./../global.module.css";
+
+const Hero = () => {
+  return (
+    <div className={classes.hero}>
+      <div className={classes.content}>
+        <p>Call us</p>
+        <p>1-800-123-4567</p>
+        <p>Because Money</p>
+        <p>Doesn't come with instructions.</p>
+        <button className={`${globalClasses.button} ${classes.button}`}>
+          Free Consultation
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
